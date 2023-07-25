ad047292bab5fff777ca09fd9ff073c7f902c7df
bhavishanth <bhavishanth@gmail.com>
2023-07-25 22:58:17 +0530

    change

--- index.js
+++ index.js
@@ -24,8 +24,12 @@ const connect = () => {
 };
 
 //middlewares
-app.use(cors({origin: "http://localhost:3000", credentials: true}));
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+  ], credentials: true, exposedHeaders: ['SET-COOKIE'],
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
